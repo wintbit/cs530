9f1f0a67dd5fafce1ac227d1158ad0854d14c47c
ramyarad <ramyarad82@gmail.com>
2015-09-24 17:46:52 -0400

    initial Application commit

---
+++ app/src/main/java/synergy/cs530/ccsu/mobileauthentication/DashBoardActivityFragment.java
@@ -0,0 +1,22 @@
+package synergy.cs530.ccsu.mobileauthentication;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DashBoardActivityFragment extends Fragment {
+
+    public DashBoardActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_dash_board, container, false);
+    }
+}
