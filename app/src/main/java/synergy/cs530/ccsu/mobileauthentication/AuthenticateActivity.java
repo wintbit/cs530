3af8d9fc50ceb2f13a567f90d37e390bb9d9676d
ejwint <ejwint@gmail.com>
2015-11-17 20:29:02 -0500

    Initial function calculation

---
+++ app/src/main/java/synergy/cs530/ccsu/mobileauthentication/AuthenticateActivity.java
@@ -0,0 +1,13 @@
+package synergy.cs530.ccsu.mobileauthentication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AuthenticateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authenicate);
+    }
+}
