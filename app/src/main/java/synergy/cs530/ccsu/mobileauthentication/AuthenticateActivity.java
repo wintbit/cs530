3382fa07c365086d318fb2f4f0667b8784cf450a
wintbit <ejwint@gmail.com>
2015-11-20 12:05:41 -0500

    Update AuthenticateActivity.java
    Need to reset the counter on the screen
    Commented code.
    Change variable names
    Removed unused variables

--- app/src/main/java/synergy/cs530/ccsu/mobileauthentication/AuthenticateActivity.java
+++ app/src/main/java/synergy/cs530/ccsu/mobileauthentication/AuthenticateActivity.java
@@ -1,96 +1,90 @@
 package synergy.cs530.ccsu.mobileauthentication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 import synergy.cs530.ccsu.mobileauthentication.dao.DatabaseManager;
-import synergy.cs530.ccsu.mobileauthentication.dao.enums.TapSequenceTableEnum;
-import synergy.cs530.ccsu.mobileauthentication.dao.models.Criterion;
-import synergy.cs530.ccsu.mobileauthentication.dao.models.DataModel;
-import synergy.cs530.ccsu.mobileauthentication.enums.NotificationEnum;
 import synergy.cs530.ccsu.mobileauthentication.utils.Algorithm;
 
 
 public class AuthenticateActivity extends AppCompatActivity implements
         View.OnClickListener, View.OnTouchListener {
 
-    private LinearLayout mLinearLayout;
-    private int currentTapCount = 0;
+    private int counter = 0;
     private DatabaseManager mDatabaseManager;
-    private HashMap<Integer, ArrayList<DataModel>> userTemplateMap
-            = new HashMap<>();
-    private ArrayList dataModel;
-    private ArrayList<Double> currentSequenceTouchDown= new ArrayList<>();
-    private ArrayList<Double> currentSequenceTouchup= new ArrayList<>();
+    private ArrayList<Double> sequenceTouchDown = new ArrayList<>();
+    private ArrayList<Double> sequenceTouchup = new ArrayList<>();
     private TextView infoTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authenicate);
-        Button mLoginButton = (Button) findViewById(R.id.activity_authenticate_login_button);
         mDatabaseManager = DatabaseManager.getInstance(getApplicationContext());
+        //Get login button
+        Button mLoginButton = (Button) findViewById(R.id.activity_authenticate_login_button);
+        //Add event listener for login button
         mLoginButton.setOnClickListener(this);
-        mLinearLayout = (LinearLayout) findViewById(R.id.activity_authenticate_linearLayout);
+        //Get the tap surface area
+        LinearLayout mLinearLayout = (LinearLayout) findViewById(
+                R.id.activity_authenticate_linearLayout);
+        //Add event listener for tap surface area
         mLinearLayout.setOnTouchListener(this);
-        infoTextView=(TextView)findViewById(R.id.activity_authenticate_info_textView);
-
+        infoTextView = (TextView) findViewById(R.id.activity_authenticate_info_textView);
     }
 
     private void incrementTap() {
-        currentTapCount++;
+        counter++;
     }
-
-
-
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    long touchDownTime = System.currentTimeMillis();
-                    currentSequenceTouchDown.add((double) touchDownTime);
-                    return true;
-                case MotionEvent.ACTION_UP:
-                    long touchUpTime = System.currentTimeMillis();
-                    currentSequenceTouchup.add((double) touchUpTime);
-
-                    incrementTap();
-                    updateTapInfoView();
-                    return true;
-
-
-            }
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                long touchDownTime = System.currentTimeMillis();
+                sequenceTouchDown.add((double) touchDownTime);
+                return true;
+            case MotionEvent.ACTION_UP:
+                long touchUpTime = System.currentTimeMillis();
+                sequenceTouchup.add((double) touchUpTime);
+                incrementTap();
+                updateTapInfoView();
+                return true;
+        }
         return false;
     }
+
     private void updateTapInfoView() {
-        infoTextView.setText(Integer.toString(currentTapCount));
+        infoTextView.setText(Integer.toString(counter));
     }
 
     @Override
     public void onClick(View v) {
-
-          Algorithm algorithm= new Algorithm(mDatabaseManager.getSequenceSetTouchDown());
-
-        Double resultTouchDown=  algorithm.compute(currentSequenceTouchDown);
-
-        algorithm= new Algorithm(mDatabaseManager.getSequenceSetTouchUp());
-
-        Double resultTouchUp=algorithm.compute(currentSequenceTouchup);
-        currentSequenceTouchDown.clear();
-        currentSequenceTouchup.clear();
+        /*Alogrithm used to compute the accuracy of user entered taps.*/
+        Algorithm algorithm = new Algorithm(mDatabaseManager.getSequenceSetTouchDown());
+        /*Comparing the user touch down*/
+        Double resultTouchDown = algorithm.compute(sequenceTouchDown);
+        algorithm = new Algorithm(mDatabaseManager.getSequenceSetTouchUp());
+        /*Comparing the user touch up*/
+        Double resultTouchUp = algorithm.compute(sequenceTouchup);
+        //Reset counter
+        counter = 0;
+        infoTextView.setText(null);
+        //Reset containers
+        sequenceTouchDown.clear();
+        sequenceTouchup.clear();
+        //Display message to user
         Toast.makeText(getApplicationContext(),
-                "Down:"+ resultTouchDown+", Up: "+
-                resultTouchUp,
+                "Down: " + resultTouchDown + ",\n" +
+                        " Up: " + resultTouchUp,
                 Toast.LENGTH_SHORT).show();
     }
 }
